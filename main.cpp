7538dc307dcc925d6fe532198244be1ca4b00f16
Patilneha024 <patilneha024@gmail.com>
2019-04-18 17:53:38 -0500

    minor changes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include "connect4game.h"
-<<<<<<< HEAD
+#include "minimaxalphabeta.h"
 #include "MinimaxAB.h"
-=======
-#include "minimaxalphabeta.h"
-#include "minimaxalphabeta.h"
->>>>>>> e4b84ffb0112e06b6c118af5f515c7a85ef72beb
+
 using namespace std;
 
 int main()
@@ -25,36 +21,12 @@ int main()
     }
     else if(choice==2)
     {
-        //AlphaBetaCode
+   //alpha beta
     }
     else
     {
         cout<<"Invalid choice";
         exit(0);
     }
-//    cout << "Test 1: filling up board with alternating pieces\n";
-//    for (int turn = 0; turn < 42; turn++)
-//    {
-//        cout<<"Turn " << turn << "\n";
-//        board.dropPiece(player,turn%7);
-//        board.printBoard();
-//        player *= -1;
-//    }
-//    cout << board.getConnectedReds();
-//    board.clearBoard();
-//    cout << "Test 2: Randomly playing 100 turns\n";
-//    for (int turn = 0; turn < 100; turn++)
-//    {
-//        cout<<"Turn " << turn << "\n";
-//        board.makeRandomMove(player);
-//        board.printBoard();
-//        player *= -1;
-//    }
-
-    // create AB object and test minmaxAB functions
-    MiniMaxAB ab;
-    // do ab.someFunction()
-
-
     return 0;
 }
